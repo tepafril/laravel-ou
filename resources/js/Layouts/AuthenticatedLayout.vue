9d9b48663fa59361d374187c4292f00925869ed0
Afril Tep <afril@wiresk.com>
2025-05-12 20:41:26 +0700

    Update Link

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -55,6 +55,13 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Result
                                 </NavLink>
+                                <a
+                                    class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                                    target="_blank"
+                                    href="https://15.235.163.68:10000/"
+                                >
+                                    Webmin
+                                </a>
                             </div>
                         </div>
 
