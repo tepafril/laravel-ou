b824c62f62589b9b725d7f84674f54d37d003473
Afril Tep <afril@wiresk.com>
2025-05-13 11:07:59 +0700

    Update abs value

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,10 +10,10 @@ class ReportController extends Controller
 {
     public function getIgameCountByF20a()
     {
-        $counts = Igame::select('f20a', DB::raw('count(*) as count'))
+        $counts = Igame::select(DB::raw('ABS(f20a) as f20a'), DB::raw('count(*) as count'))
             ->whereNotNull('f20a')
-            ->groupBy('f20a')
-            ->orderBy('f20a')
+            ->groupBy(DB::raw('ABS(f20a)'))
+            ->orderBy(DB::raw('ABS(f20a)'))
             ->get();
 
         return response()->json($counts);
